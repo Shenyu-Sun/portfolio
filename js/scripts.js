f1ae7fa376393586ac1fa626a96d073357096fe7
Shenyu <sectious@gmail.com>
2022-05-14 21:16:34 +0300

    Fix the toggle problem

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
 console.log("Hi, This is Shenyu. Welcome to my Portfolio Website!");
 
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
