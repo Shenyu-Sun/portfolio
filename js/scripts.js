5c7dfcb9731f26be11680864e6b602dd0d8f6586
Shenyu <sectious@gmail.com>
2022-05-18 17:49:54 +0300

    Fixed the visited link color

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 console.log("Hi, This is Shenyu. Welcome to my Portfolio Website!");
 
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
+  var x = document.getElementById("myNavtoggle");
+  if (x.className === "navtoggle") {
+    x.className += " responsive";
+  } else {
+    x.className = "navtoggle";
   }
+}
